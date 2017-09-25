cb53f13d128871f13007932950a60da0588a8f85
hang <hang22c@gmail.com>
2017-09-25 18:17:04 -0400

    ADD order to parser

--- squawka-scraper/parser.rb
+++ squawka-scraper/parser.rb
@@ -6,17 +6,21 @@ TIMESLICE_EVENT_KEYS = ["mins", "minsec", "player_id", "secs", "competition", "k
 
 def parse
 	data = {}
+	data['timeslice_events'] = []
 	Dir[DATA_PATH].each do |file_path|
 		data_type = file_path.split('/').last.split('.').first
-		data['timeslice_events'] = []
 		CSV.foreach(file_path, headers: true) do |row|
 			row_data = row.to_hash
 			if row_data['minsec']
-				data['timeslice_events'] << TIMESLICE_EVENT_KEYS.map {|key| [key, row_data.delete(key)]}.to_h.merge('data' => row_data)
+				data['timeslice_events'] << (TIMESLICE_EVENT_KEYS.map {|key| [key, row_data.delete(key)]}.to_h.merge('data' => row_data, 'data_type' => data_type))
 			else
 				data[data_type] = row_data
 			end
 		end
+	end
+	data['timeslice_events'] = data['timeslice_events'].sort_by {|d| d['minsec'].to_i }
+	data['timeslice_events'].each do |d|
+		p d
 	end
 end
 
